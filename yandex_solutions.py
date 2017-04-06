933dd47c16d684d43289d5d57bd58fcdd1ece6e5
Kaftanov <ilyakaftanovn@gmail.com>
2017-04-06 20:37:39 +0300

    Added 4 task in yandex_solutions.py

--- yandex_solutions.py
+++ yandex_solutions.py
@@ -60,10 +60,53 @@ class MoviesLib(object):
         return random.sample(self.movies_set, 1)
 
 
-def class_test():
+def class_hand():
     obj = MoviesLib('Sherlock', 'Fight Club', 'Fate/Zero')
     obj.append('La La Land')
     obj.remove_movie('Sherlock')
     obj.append('Ghost In The Shell')
     print(obj.get_randmovie())
     obj.print_lib()
+
+
+class Tree(object):
+    values_list = []
+
+    def __init__(self, val, left_node=None, right_node=None):
+        if val is None:
+            raise Exception('value is empty')
+        else:
+            self.val = val
+        self.left_node = left_node
+        self.right_node = right_node
+        self.values_list.append(self.val)
+
+    @staticmethod
+    def check_sub_tree(object):
+        return True if type(object) is Tree else False
+
+    def searching_nodes(self, tree):
+        if self.check_sub_tree(tree.right_node):
+            self.searching_nodes(tree.right_node)
+        else:
+            if tree.right_node is not None:
+                self.values_list.append(tree.right_node)
+        if self.check_sub_tree(tree.left_node):
+            self.searching_nodes(tree.left_node)
+        else:
+            if tree.left_node is not None:
+                self.values_list.append(tree.left_node)
+
+    def most_common(self):
+        """ 'все наиболее часто встречающиеся элементы'
+            я вывел все всеречающиеся элементы по убыванию
+            можно выбрать нужное кол-во остальное отсеять
+        """
+        readable_dict = {}
+        for i in Counter(self.values_list).most_common():
+            readable_dict[i[0]] = i[1]
+        return readable_dict
+
+obj = Tree(1, Tree(1, Tree(1, 1, Tree(3, 2, 6)), Tree(2, 2, 6)), Tree(2, Tree(2, 1), Tree(3, 2, 6)))
+obj.searching_nodes(obj)
+print(obj.most_common())
